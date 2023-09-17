c72031ab9ffbfea14bc8bfd86d4308e1a8f45115
Pratham Choudhary <prathamchoudhary924@gmail.com>
2023-09-17 21:45:19 +0530

    Updated some files

--- backend/index.js
+++ backend/index.js
@@ -11,4 +11,12 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 app.use(bodyParser.json());
 app.use(cors());
-app.use(cookieParser());;
+
+
+app.get("/", (req, res) =>{
+    res.send("Welcome to blog app!");
+});
+// dbConnect();
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
