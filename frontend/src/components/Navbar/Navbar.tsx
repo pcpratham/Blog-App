2999a37e1f092e53db3be65b1dea2ba26c91cf4a
Pratham Choudhary <prathamchoudhary924@gmail.com>
2023-09-18 01:41:53 +0530

    Navbar setup done

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import {BiUserCircle,BiPlus} from "react-icons/bi"
-import Link from 'next/link'
-
-
+import React from "react";
+import { BiUserCircle, BiPlus } from "react-icons/bi";
+import { AiOutlineSearch } from "react-icons/ai";
+import Link from "next/link";
+import logo from "@/assets/blogger-2.png";
+import Image from "next/image";
+import './Navbar.css'
 const Navbar = () => {
   return (
-    <nav className='navbar'>
-        <div className='navbar-left'>
-            <Link href="/profile">
-                <BiUserCircle classname="icon" />
-            </Link>
-            <Link href="/add" >
-                <BiPlus className="icon" />
-            </Link>
-        </div>
-        <div className='navbar-middle'></div>
-        <div className='navbar-right'></div>
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link href="/profile">
+          <BiUserCircle className="icon" />
+        </Link>
+        <Link href="/add">
+          <BiPlus className="icon" />
+        </Link>
+        <Link href="/search">
+          <AiOutlineSearch className="icon" />
+        </Link>
+      </div>
+      <div className="navbar-middle">
+        <Link href="/">
+          <Image src={logo} alt="logo" />
+        </Link>
+      </div>
+      <div className="navbar-right">
+        <Link href="/">Home</Link>
+        <Link href="/pages/about">About</Link>
+        <Link href="/pages/contact">Contact</Link>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
